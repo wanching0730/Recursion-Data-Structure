854c19965b8dc102c8cf81c704b797efee369f0f
wanching_97 <wanching_97@hotmail.com>
2018-06-12 12:41:39 +0800

    added factorial method

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,2 +1,12 @@
 public class Factorial {
+    public static void main(String[] args) {
+        System.out.println("Factorial is "+ factorial(3));
+    }
+
+    public static long factorial(int number) {
+        if(number == 0)
+            return 1;
+        else
+            return number * factorial(number - 1);
+    }
 }
